6474b4eea3bafed973286f393ca47107dc500788
GabiJoita <GabiJoita191@gmail.com>
2024-05-08 07:17:39 +0300

    grocery_list_app.py

--- grocery_list_app.py
+++ grocery_list_app.py
@@ -2,7 +2,9 @@
 
 import tkinter as tk
 import csv
+import pandas as pd
 import customtkinter as cst
+from customtkinter import *
 from tkinter import messagebox
 from list_data import MyDbList
 
@@ -13,12 +15,14 @@ def add_product():
         than save the list in a csv file
      """
     product = product_entry.get()
-    list_prod.insert(tk.END, f'{product} lei')
+    price = price_entry.get()
+    list_prod.insert(tk.END, f'{product} {price} lei')
     product_entry.delete(0, tk.END)
+    price_entry.delete(0, tk.END)
     if product:
         with open('grocery_list.csv', 'a', newline="") as fr:
             writer = csv.writer(fr)
-            writer.writerow([product])
+            writer.writerow([product, price])
     else:
         messagebox.showerror(message='please, enter a product')
 
@@ -35,12 +39,21 @@ def total_price():
     return total
 
 
-# def prod_categories():
-#     categories = {}
-#     for item in list_prod.get(0, tk.END):
-#         category = str(item.split()[-3])
-#         categories.append(category)
-#     return categories
+def prod_categories():
+    categories = []
+    for item in list_prod.get(0, tk.END):
+        data = item.split()
+        data_cat = {
+            'id': data[0],
+            'name': data[1],
+            'category': data[2],
+            'price': float(data[3]),
+            'value': data[4]
+        }
+        categories.append(data_cat)
+    df_cat = pd.DataFrame(categories)
+    print(df_cat.groupby('category'))
+    return df_cat.groupby('category')
 
 
 def show_total_price():
@@ -56,14 +69,18 @@ def show_total_price():
     index.set(s_tot)
 
 
-# def show_categories():
-#     s_cat = prod_categories()
-#     top = tk.Toplevel()
-#     top.configure(bg='#857EFF')
-#     top.geometry('200x100')
-#     label = tk.Label(top, text='', textvariable=index, bg='#857EFF')
-#     label.pack()
-#     index.set(s_cat)
+def show_categories():
+    s_cat = prod_categories()
+    price_cat = ''
+    for cat in s_cat:
+        price_cat += f"{cat[0]}: {cat[1]['price'].sum()}\n"
+        print(cat[1])
+    top = tk.Toplevel()
+    top.configure(bg='#857EFF')
+    top.geometry('200x100')
+    label = tk.Label(top, text='', textvariable=index, bg='#857EFF')
+    label.pack()
+    index.set(price_cat)
 
 
 def delete_product():
@@ -96,8 +113,14 @@ if __name__ == "__main__":
     product_text = tk.Label(root, text='enter a product:', background='#1B0A6B', foreground='white')
     product_text.grid(row=0, column=0, padx=10, pady=10)
 
-    product_entry = tk.Entry(root, width=30)
+    product_entry = CTkEntry(root, width=200)
     product_entry.grid(row=0, column=1, padx=10, pady=10)
+
+    price_text = tk.Label(root, text='enter a price:', background='#1B0A6B', foreground='white')
+    price_text.grid(row=1, column=0, padx=10, pady=10)
+
+    price_entry = CTkEntry(root, width=200)
+    price_entry.grid(row=1, column=1, padx=10, pady=10)
 
     product_button = cst.CTkButton(root, text='add product',
                                    corner_radius=32, width=10, height=25, command=add_product)
@@ -113,7 +136,7 @@ if __name__ == "__main__":
     scrollbar = tk.Scrollbar(list_frame)
     scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
-    list_prod = tk.Listbox(list_frame, selectmode=tk.EXTENDED,
+    list_prod = tk.Listbox(list_frame, bg="#383838", fg="#56F203", selectmode=tk.EXTENDED,
                            width=50, height=18, yscrollcommand=scrollbar.set)
     list_prod.pack(side=tk.LEFT, fill=tk.BOTH)
 
@@ -127,8 +150,8 @@ if __name__ == "__main__":
                                  command=show_total_price)
     total_button.grid(row=2, column=3)
 
-    # categories_button = cst.CTkButton(root, text='show categories', corner_radius=32, width=15, height=30,
-    #                                   command=show_categories)
-    # categories_button.grid(row=3, column=3)
+    categories_button = cst.CTkButton(root, text='show categories', corner_radius=32, width=15, height=30,
+                                      command=show_categories)
+    categories_button.grid(row=3, column=3)
 
     root.mainloop()
